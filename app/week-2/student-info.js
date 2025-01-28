1e8bdcd1a193fc218bcde7404a37eefff8fef11f
CraftyNuker <gallasicnoah@gmail.com>
2025-01-28 03:10:12 -0700

    Added week-2 folder & js files
    Added page.js & student-info.js

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <h1>Noah Gallasic</h1>
+        <link href="https://github.com/ChronicCoffee">Personal Github Page</link>
+      </main>
+    );
+  }
