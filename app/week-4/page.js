678f14b182396794e1685ab8cdc190030392314d
CraftyNuker <gallasicnoah@gmail.com>
2025-02-11 16:52:05 -0700

    Updated Week 4 Folder
    updated new-item.js & page.js

---
+++ app/week-4/page.js
@@ -0,0 +1,10 @@
+// /app/week-4/page.js
+import NewItem from './new-item';
+
+export default function Week4Page() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-black via-indigo-950 to-black p-4 flex items-center justify-center">
+      <NewItem />
+    </div>
+  );
+}
