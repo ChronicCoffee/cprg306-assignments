7794a9f4d1a14c9527885fde9b06ea8519acd498
CraftyNuker <gallasicnoah@gmail.com>
2025-03-02 01:24:43 -0700

    Started week-6
    Started on week-6 assignment

--- app/page.js
+++ app/page.js
@@ -17,6 +17,9 @@ export default function Page(){
       <p>
       Link to Week 5's Assignment: <Link href="/week-5">Week 5</Link>
       </p>
+      <p>
+      Link to Week 6's Assignment: <Link href="/week-6">Week 6</Link>
+      </p>
       </div>
     </main>
   );
