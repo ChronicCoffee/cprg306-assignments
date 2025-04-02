3f27ee14927e652bd80e07db0c8c6f3a4f72cba7
CraftyNuker <gallasicnoah@gmail.com>
2025-04-01 23:27:58 -0600

    Updated Pages
    Updated links to week-10 instead of week-9

--- app/page.js
+++ app/page.js
@@ -1,37 +1,37 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-export default function Page(){
+export default function Page() {
   return (
     <main>
       <div className="min-h-screen bg-gradient-to-br from-black via-indigo-950 to-black p-4">
-      <h1>CPRG-306 - Web Development 2: Assignments</h1>
-      <p>
-      Link to Week 2's Assignment: <Link href="/week-2">Week 2</Link>
-      </p>
-      <p>
-      Link to Week 3's Assignment: <Link href="/week-3">Week 3</Link>
-      </p>
-      <p>
-      Link to Week 4's Assignment: <Link href="/week-4">Week 4</Link>
-      </p>
-      <p>
-      Link to Week 5's Assignment: <Link href="/week-5">Week 5</Link>
-      </p>
-      <p>
-      Link to Week 6's Assignment: <Link href="/week-6">Week 6</Link>
-      </p>
-      <p>
-      Link to Week 7's Assignment: <Link href="/week-7">Week 7</Link>
-      </p>
-      <p>
-      Link to Week 8's Assignment: <Link href="/week-8">Week 8</Link>
-      </p>
-      <p>
-      Link to Week 9's Assignment: <Link href="/week-9">Week 9</Link>
-      </p>
-      <p>
-      Link to Week 10's Assignment: <Link href="/week-9">Week 10</Link>
-      </p>
+        <h1>CPRG-306 - Web Development 2: Assignments</h1>
+        <p>
+          Link to Week 2's Assignment: <Link href="/week-2">Week 2</Link>
+        </p>
+        <p>
+          Link to Week 3's Assignment: <Link href="/week-3">Week 3</Link>
+        </p>
+        <p>
+          Link to Week 4's Assignment: <Link href="/week-4">Week 4</Link>
+        </p>
+        <p>
+          Link to Week 5's Assignment: <Link href="/week-5">Week 5</Link>
+        </p>
+        <p>
+          Link to Week 6's Assignment: <Link href="/week-6">Week 6</Link>
+        </p>
+        <p>
+          Link to Week 7's Assignment: <Link href="/week-7">Week 7</Link>
+        </p>
+        <p>
+          Link to Week 8's Assignment: <Link href="/week-8">Week 8</Link>
+        </p>
+        <p>
+          Link to Week 9's Assignment: <Link href="/week-9">Week 9</Link>
+        </p>
+        <p>
+          Link to Week 10's Assignment: <Link href="/week-10">Week 10</Link>
+        </p>
       </div>
     </main>
   );
--- app/week-10/shopping-list/_services/shopping-list-service.js
+++ app/week-10/shopping-list/_services/shopping-list-service.js
@@ -1,4 +1,4 @@
-import { db } from "../_utils/firebase";
+import { db } from "../../_utils/firebase";
 import { collection, getDocs, addDoc, query, where, doc, deleteDoc } from "firebase/firestore";
 
 export async function getItems(userId) {
